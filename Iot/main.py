049efe30cf641c146ac8aa798aebf136cb8fc3b5
Trungdung090 <168762836+Trungdung090@users.noreply.github.com>
2025-02-25 01:19:04 +0700

    Delete Iot directory

--- Iot/main.py
+++
@@ -1,7 +0,0 @@
-from ultralytics import YOLO
-
-# Create a new YOLO model from scratch
-model = YOLO('yolov8m.pt')
-
-# Train the model using the 'coco8.yaml' dataset for 3 epochs
-results = model.train(data='data.yaml', epochs=10, imgsz=640, batch=16)
